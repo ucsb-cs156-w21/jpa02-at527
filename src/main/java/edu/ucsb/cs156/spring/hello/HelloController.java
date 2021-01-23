e63b81e027d0655e134dd01ffc8650137ab68151
Andrew Tran <andrew_tran@ucsb.edu>
2021-01-23 04:50:31 -0800

    at - changed hello name to conform to gradescope (no underscores)

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from andrew_tran!";
+        return "Greetings from andrew-tran!";
     }
     
 }
